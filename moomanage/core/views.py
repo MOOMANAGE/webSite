aff653bbdc1f6876b0a11d4901b11c5c59ed6fde
andres yair carvajal bolivar <ancarvajalb@unal.edu.co>
2023-07-10 05:51:59 -0500

    estructura nueva de base

--- moomanage/core/views.py
+++ moomanage/core/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def frontpage(request):
-    return render(request,'core/base.html')
+    return render(request,'core/frontpage.html')
+def about(request):
+    return render(request,'core/about.html')
