136a0dd268cd4662e7150148bb0d625e86164359
andres yair carvajal bolivar <ancarvajalb@unal.edu.co>
2023-07-17 14:48:38 -0500

    plan b-1

--- moomanage/moomanage/settings.py
+++ moomanage/moomanage/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["snoopbrain.pythonanywhere.com","127.0.0.1"]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root/')
 
 STATICFILES_DIRS=[BASE_DIR / "static",]
