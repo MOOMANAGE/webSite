5cbd377e2b687b00c1ff36bf48b35c9a9e7258c8
andres yair carvajal bolivar <ancarvajalb@unal.edu.co>
2023-08-11 20:41:56 -0500

    eliminacion de host

--- moomanage/core/views.py
+++ moomanage/core/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def frontpage(request):
-    return render(request,'core/frontpage.html')
+    return render(request,'core/base.html')
 def about(request):
     return render(request,'core/about.html')
 def servicios(request):
